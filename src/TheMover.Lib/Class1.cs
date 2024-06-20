50415a4388f81b564780228ae6523d42591b68d8
jojochr <jojochr@users.noreply.github.com>
2024-06-20 16:50:43 +0200

    C#- Solution und Projekte erstellt

---
+++ src/TheMover.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMover.Lib
+{
+    public class Class1
+    {
+    }
+}
