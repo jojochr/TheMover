eb61f5c4e1757f72861d8c49fbdee88568c05d5b
jojochr <jojochr@users.noreply.github.com>
2024-11-16 15:58:24 +0100

    Minimal refactoring to entry point

--- src/TheMover.UI/Program.cs
+++ src/TheMover.UI/Program.cs
@@ -3,18 +3,20 @@ using System;
 
 namespace TheMover.UI;
 
-sealed class Program {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+public static class Program {
+    /// <summary>
+    /// Initialization code. Don't use any Avalonia, third-party APIs or any
+    /// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    /// yet and stuff might break.
+    /// </summary>
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-       .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-                     .UsePlatformDetect()
-                     .WithInterFont()
-                     .LogToTrace();
+    /// <summary>Avalonia configuration, don't remove; also used by visual designer.</summary>
+    private static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+                                                             .UsePlatformDetect()
+                                                             .WithInterFont()
+                                                             .LogToTrace();
 }
--- src/TheMover.UI/ViewLocator.cs
+++ src/TheMover.UI/ViewLocator.cs
@@ -6,21 +6,21 @@ using TheMover.UI.ViewModels;
 namespace TheMover.UI;
 
 public class ViewLocator : IDataTemplate {
-
     public Control? Build(object? data) {
-        if(data is null)
+        if(data is null) {
             return null;
-
-        var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
-
-        if(type != null) {
-            var control = (Control)Activator.CreateInstance(type)!;
-            control.DataContext = data;
-            return control;
         }
 
-        return new TextBlock { Text = "Not Found: " + name };
+        string name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
+
+        if(type is null) {
+            return new TextBlock { Text = "Not Found: " + name, };
+        }
+
+        var control = (Control)Activator.CreateInstance(type)!;
+        control.DataContext = data;
+        return control;
     }
 
     public bool Match(object? data) {
