a974516a25add38c5340a7665b2b397b3236447e
Johannes Christel <johannes.christel@outlook.de>
2025-01-26 18:09:34 +0100

    Added Scalar API-Frontend and wrote some actual frontend to Program.cs

--- src/TheMover.API/Program.cs
+++ src/TheMover.API/Program.cs
@@ -1,15 +1,47 @@
+using Scalar.AspNetCore;
 using TheMover.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+if (app.Environment.IsDevelopment())
+{
+    // Creates API Specification
+    app.MapOpenApi();
+    
+    // Consumes the spec and generates a web frontend
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("TheMover API")
+            .WithTheme(ScalarTheme.Moon)
+            .WithDarkMode(darkMode: true)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+    
+    // Add grpc reflection for local development
+    app.MapGrpcReflectionService();
+}
+
+// Create gRPC Endpoints
+app.MapGrpcService<GreeterService>()
+    .RequireHost("*:5001");
+
+CreateRestEndpoints(app);
+
 app.Run();
+
+return;
+
+static void CreateRestEndpoints(WebApplication app)
+{
+    app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909")
+        .WithName("TestEndpoint")
+        .RequireHost("*:5000");
+}
