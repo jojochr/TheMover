d394c1ab1b47e42a8a6f671c00ab52cb77d05e30
Johannes Christel <johannes.christel@outlook.de>
2025-02-08 00:49:51 +0100

    Dockerized Astro WebApp

--- src/TheMover.Web/astro.config.mjs
+++ src/TheMover.Web/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  })
+    adapter: node({
+        mode: 'standalone'
+    }),
+    server: {
+        port: 4999,
+        host: "0.0.0.0"
+    }
 });
