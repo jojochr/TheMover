8b6a37378baa3bb17ab046f8f2ed62a8dee122b4
Johannes Christel <johannes.christel@outlook.de>
2025-03-14 09:50:52 +0100

    Added brace styling to editorconfig

--- src/TheMover.API/Program.cs
+++ src/TheMover.API/Program.cs
@@ -15,21 +15,19 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     // Creates API Specification
     app.MapOpenApi();
-    
+
     // Consumes the spec and generates a web frontend
-    app.MapScalarApiReference(options =>
-    {
+    app.MapScalarApiReference(options => {
         options
             .WithTitle("TheMover API")
             .WithTheme(ScalarTheme.Moon)
             .WithDarkMode(darkMode: true)
             .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
     });
-    
+
     // Add grpc reflection for local development
     app.MapGrpcReflectionService();
 }
@@ -44,8 +42,7 @@ app.Run();
 
 return;
 
-static void CreateRestEndpoints(WebApplication app)
-{
+static void CreateRestEndpoints(WebApplication app) {
     app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909")
         .RequireHost("*:5000", "api.themover.site");
 }
--- src/TheMover.API/Services/GreeterService.cs
+++ src/TheMover.API/Services/GreeterService.cs
@@ -3,19 +3,15 @@ using TheMover.API;
 
 namespace TheMover.API.Services;
 
-public class GreeterService : Greeter.GreeterBase
-{
+public class GreeterService : Greeter.GreeterBase {
     private readonly ILogger<GreeterService> _logger;
 
-    public GreeterService(ILogger<GreeterService> logger)
-    {
+    public GreeterService(ILogger<GreeterService> logger) {
         _logger = logger;
     }
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-        return Task.FromResult(new HelloReply
-        {
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
+        return Task.FromResult(new HelloReply {
             Message = "Hello " + request.Name
         });
     }
