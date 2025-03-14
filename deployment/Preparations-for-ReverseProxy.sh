a83de5c870a22a89d3725780ed8ecca70f738836
Johannes Christel <johannes.christel@outlook.de>
2025-03-14 17:45:01 +0100

    Created Single scirpt for network and volume

--- deployment/Preparations-for-ReverseProxy.sh
+++ deployment/Preparations-for-ReverseProxy.sh
@@ -3,9 +3,9 @@
 # Set -e to exit immediately if a command exits with a non-zero status.
 set -e
 
-VOLUME_NAME="TheMover_traefik_certs"
 
 echo "Checking if certificate volume exists..."
+VOLUME_NAME="traefik_certs"
 MAYBE_EXISTING_VOLUME=( $(docker volume ls --filter name=$VOLUME_NAME -q) )
 if [[ ${#MAYBE_EXISTING_VOLUME[@]} -gt 0 ]]
 then
@@ -17,6 +17,22 @@ else
     --scope single \
     --sharing all \
     $VOLUME_NAME
+fi
+
+echo ""
+echo "Checking if network for reverse proxy exists..."
+NETWORK_NAME="reverse_proxy"
+MAYBE_EXISTING_NETWORK=( $(docker network ls --filter name=$NETWORK_NAME -q) )
+if [[ ${#MAYBE_EXISTING_NETWORK[@]} -gt 0 ]]
+then
+  echo "The docker volume \"$NETWORK_NAME\" already exists."
+else
+  echo "Creating docker network \"$NETWORK_NAME\"..."
+  docker network create \
+    --driver overlay \
+    --scope swarm \
+    --attachable \
+    $NETWORK_NAME
 fi
 
 exit 0
